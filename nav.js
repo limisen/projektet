bb7d16a6ab93b835711d54991abf52156db72dc1
limisen <liam.cosford@edu.nykoping.se>
2021-03-25 21:32:25 +0100

    did some late night work, trying to add a nave,slide bar with java scrip and added a pic

---
+++ nav.js
@@ -0,0 +1,15 @@
+'use strict'
+
+let nav = document.getElementById('nav')
+let x = document.getElementById('x')
+
+x.addEventListener('click', function() {
+    console.log("CLICKAD PÅ!!!");
+    nav.classList.toggle('open')
+})
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === "Escape" && nav.classList.contains('open')) { 
+        nav.classList.toggle('open')
+    }
+})
