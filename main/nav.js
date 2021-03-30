2e3011c7790c84a38cbc1ee11ef06e7692165688
limisen <liam.cosford@edu.nykoping.se>
2021-03-30 14:57:37 +0200

    further progress

--- main/nav.js
+++ main/nav.js
@@ -6,15 +6,11 @@ let nav = document.getElementById('nav')
 x.addEventListener('click', function () {
     console.log("CLICKAD PÅ!!!");
     nav.classList.toggle('open')
-})
-
-sub_nav.addEventListener('click', function () {
-    console.log("CLICKADE PÅ Sub_nav!!!");
-    nav.classList.toggle('open')
+    span.contains('>'); span.cotent.toggle('<')
 })
 
 document.addEventListener('keydown', function (event) {
     if (event.key === "Escape" && nav.classList.contains('open')) {
-        nav.classList.toggle('open')
+        nav.classList.toggle('open');
     }
 })
--- main/sub_nav.js
+++ main/sub_nav.js
@@ -1,7 +1,13 @@
 'use strict'
 
 let sub_nav = document.getElementById("sub_nav")
-let sub_maps = document.getElementById('sub_maps')
+let sub_maps = document.getElementById('sub_maps_hidw')
+
+
+sub_nav.addEventListener('click', function () {
+    console.log("CLICKADE PÅ Sub_nav!!!");
+    nav.classList.toggle('open')
+})
 
 sub_maps.addEventListener('on_cursor_hover', (instance) => {
     sub_maps.onmouseover(
